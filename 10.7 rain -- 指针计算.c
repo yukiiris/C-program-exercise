bbdc17bc8bb070e88fa051fb966b05de093e1c36
yukiiris <25852469@qq.com>
2016-11-11 20:59:41 +0800

    exercise

---
+++ 10.7 rain -- 指针计算.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#define MOUTHS 12
+#define YEARS 5
+int main(void)
+{
+	const float rain[YEARS][MOUTHS] = 
+	{
+		{ 4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6 },
+		{ 8.5, 8.2, 1.2, 1.6, 2.4, 0.0, 5.2, 0.9, 0.3, 0.9, 1.4, 7.3 },
+		{ 9.1, 8.5, 6.7, 4.3, 2.1, 0.8, 0.2, 0.2, 1.1, 2.3, 6.1, 8.4 },
+		{ 7.2, 9.9, 8.4, 3.3, 1.2, 0.8, 0.4, 0.0, 0.6, 1.7, 4.3, 6.2 },
+		{ 7.6, 5.6, 3.8, 2.8, 3.8, 0.2, 0.0, 0.0, 0.0, 1.3, 2.6, 5.2 }
+	};
+	int mouth, year;
+	float subtot, total;
+
+
+	printf(" YEAR    RAINFALL   (inches)\n");
+	for (year = 0, subtot = 0; year < YEARS; year++)
+	{
+		for (mouth = 0, total = 0; mouth < MOUTHS; mouth++)
+			subtot += *(*(rain+year) + mouth);
+		printf("%5d %15.1f\n", 2010 + year, subtot);
+		total += subtot;
+	}
+
+	printf("\nThe years average is %.1f inches.\n", total / YEARS);
+	printf("Mouthly average is :\n");
+	printf(" Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec\n");
+
+	for (mouth = 0; mouth < MOUTHS; mouth++)
+	{
+		for (year = 0, subtot = 0; year < YEARS; year++)
+			subtot += *(*(rain+year) + mouth);
+		printf("%4.1f ", subtot / YEARS);
+	}
+	printf("\n");
+
+	return 0;
+}
+
+
+
